25fd11076b923b3f607f29d0a96c70b4e3c44e40
Master Sofua Desmond <mrsofua@gmail.com>
2021-10-26 15:44:39 +0000

    splash screen constructed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/screens/splash/splash.dart';
+import 'package:food_app/utils/size_config.dart';
 
 void main() {
-  runApp(MaterialApp(
-
-  ));
+  runApp(MediaQ());
 }
 
+class MediaQ extends StatelessWidget {
+  const MediaQ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return MaterialApp(
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          fontFamily: 'Gilroy',
+          appBarTheme: AppBarTheme(
+              brightness: Brightness.light,
+              backgroundColor: Colors.white,
+              elevation: 0.0)),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Splash(),
+      },
+    );
+  }
+}
--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Splash extends StatelessWidget {
   const Splash({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF53B175),
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset('assets/images/Groupcarrot.svg'),
+              SizedBox(
+                width: 18.36,
+              ),
+              Column(
+                children: [
+                  SvgPicture.asset('assets/images/Groupnectar.svg'),
+                  Text(
+                    'online groceriet',
+                    style: TextStyle(
+                        fontSize: 18, color: Colors.white, letterSpacing: 5.5),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 110.0,
+          )
+        ],
+      ),
+    );
   }
 }
